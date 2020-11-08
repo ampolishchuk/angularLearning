009c97a99823b8c46ab2edcbeaa285617deed247
Alexey Polishchuk <am.polishchuk@gmail.com>
2020-11-08 22:56:03 +0300

    Part 3. Lesson 1 after Prettier

---
+++ src/app/post-form/post-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.scss'],
+})
+export class PostFormComponent implements OnInit {
+  // tslint:disable-next-line:no-empty
+  constructor() {}
+
+  // tslint:disable-next-line:no-empty
+  ngOnInit(): void {}
+}
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+})
+export class PostComponent implements OnInit {
+  // tslint:disable-next-line:no-empty
+  constructor() {}
+
+  // tslint:disable-next-line:no-empty
+  ngOnInit(): void {}
+}
